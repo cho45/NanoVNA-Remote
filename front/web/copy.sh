cc243a983becc01218b28d48eece7e305512a1f9
cho45 <cho45@lowreal.net>
2020-11-08 12:34:30 +0900

    do not store filename/timestamp for gzip-ed files

--- front/web/copy.sh
+++ front/web/copy.sh
@@ -20,12 +20,12 @@ cd ..
 
 rm -rf dist
 mkdir dist
-cp -RL NanoVNA-Web-Client/www/* dist/
+cp -pRL NanoVNA-Web-Client/www/* dist/
 mv dist/lib/material-design-icons-iconfont dist/lib/mdii
 sed -i "" s/material-design-icons-iconfont/mdii/ dist/index.html
 sed -i "" 's!<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic">.*!!' dist/index.html
 sed -i "" 's!<script src="cordova.js" defer></script>!!' dist/index.html
-find dist -type f | xargs gzip
+find dist -type f | xargs gzip -n
 find dist -print
 du -h dist
 
