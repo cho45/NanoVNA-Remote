b50d577c4e633fa6996bcbb69b9fed9033e42684
cho45 <cho45@lowreal.net>
2020-11-08 12:28:20 +0900

    copy.sh

--- front/web/copy.sh
+++ front/web/copy.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
 set -ex
+
+cd `git rev-parse --show-toplevel`
+cd front/web
 echo $PWD
-rm -r dist
+
+if [ ! -e NanoVNA-Web-Client ]; then
+	git clone https://github.com/cho45/NanoVNA-Web-Client.git
+	cd NanoVNA-Web-Client
+	git checkout websocket
+	cd ..
+fi
+
+cd NanoVNA-Web-Client
+git pull
+./copy-minimum.sh
+cd ..
+
+rm -rf dist
 mkdir dist
-cp -RL $HOME/project/NanoVNA-Web-Client/www/* dist/
+cp -RL NanoVNA-Web-Client/www/* dist/
 mv dist/lib/material-design-icons-iconfont dist/lib/mdii
 sed -i "" s/material-design-icons-iconfont/mdii/ dist/index.html
 sed -i "" 's!<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic">.*!!' dist/index.html
@@ -12,3 +28,4 @@ sed -i "" 's!<script src="cordova.js" defer></script>!!' dist/index.html
 find dist -type f | xargs gzip
 find dist -print
 du -h dist
+
